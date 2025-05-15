a67097b1d51593aa080e130137a746acb6653e08
Jonathan Hansen <jhansen@flexpay.io>
2025-05-15 12:31:57 -0700

    test

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "swagger.yaml",
+    url: [ { url: "swagger.yaml", name: "Test" }, { url: "swagger.yaml", name: "Test2" }],
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
